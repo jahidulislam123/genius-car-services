daa6163f42b88dde626a1b05c7d8a2f80d7686f5
unknown <islam15-3879@diu.edu.bd>
2022-04-15 00:23:03 +0600

    added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import repair1 from '../../../images/repair1.jpg'
+
+const services=[
+    {id:1 ,name: 'oil change ', price : 100,description: "",img:repair1}
+]
+
+
 const Services = () => {
     return (
         <div>
