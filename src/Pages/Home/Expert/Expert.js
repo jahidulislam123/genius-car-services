2d62a55cb8b0abfafc497afc49c793ff6f989491
unknown <islam15-3879@diu.edu.bd>
2022-04-15 19:51:19 +0600

    added

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Expert = ({expert}) => {
     const {name,img}=expert;
     return (
-        <div class="card gx-5 col-sm-12 col-md-6 col-lg-4" style={{width: '18rem'}}>
+        <div class=' col-sm-12 col-md-6 g-5 col-lg-4'>
+
+<div class="card " style={{width: '18rem'}}>
   <img src={img} class="card-img-top" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">{name}</h5>
@@ -11,6 +13,8 @@ const Expert = ({expert}) => {
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
+        </div>
+        
     );
 };
 
