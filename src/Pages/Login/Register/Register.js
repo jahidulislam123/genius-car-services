df044095c77da81b6b39199aca96e45bc554b592
unknown <islam15-3879@diu.edu.bd>
2022-04-17 20:49:14 +0600

    authentication-hook

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+
+
     const navigate =useNavigate()
     const navigateLogin =()=>{
         navigate('/login')
     }
 
     const handleRegister =(event)=>{
+
+       
         event.preventDefault();
         const name=event.target.name.value;
-        const eamil=event.target.email.value;
+        const email=event.target.email.value;
         const password=event.target.password.value;
+        
+        createUserWithEmailAndPassword(email,password);    
 
+    }
+    if(user){
+        navigate('/home');
     }
 
     return (
