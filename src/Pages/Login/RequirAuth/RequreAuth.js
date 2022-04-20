f1392d5c6beabb6e9cca2d3c5b83213fff430df0
unknown <islam15-3879@diu.edu.bd>
2022-04-20 17:29:05 +0600

    email-verification

--- src/Pages/Login/RequirAuth/RequreAuth.js
+++ src/Pages/Login/RequirAuth/RequreAuth.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
 const RequreAuth = ({children}) => {
-
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const [user,loading] = useAuthState(auth);
     
     const location=useLocation();
     if(loading){
         <Loading></Loading>
     }
+    
     if(!user){
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
+    if(!user.emailVerified){
+        return <div>
+            <h3 className='text-danger'>Your Email is not varifired
+            </h3>
+            <h5 className='text-success'>Please verify your Email address</h5>
+         <button className='btn btn-primary'
+        onClick={async () => {
+          await sendEmailVerification();
+          toast('Sent email');
+        }}
+      >
+       sent Verification email email 
+      </button>
+      <ToastContainer></ToastContainer>
+        </div>
+        
+    }
+    
     return children;
 };
 
