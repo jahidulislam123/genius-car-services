32277e55017480ebefbc7945e7fca04f3ed31cc0
unknown <islam15-3879@diu.edu.bd>
2022-05-01 08:26:03 +0600

    added-api-axios

--- src/Pages/CheackOut/CheackOut/Cheackout.js
+++ src/Pages/CheackOut/CheackOut/Cheackout.js
@@ -1,38 +1,55 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetails from '../../../hooks/useServiceDetails';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Cheackout = () => {
     const {servicesId}=useParams();
     const [service]=useServiceDetails(servicesId);
-    const [user,setUser]=useState({
-        name: 'Akbar the great ',
-        email: "akbar@moomo.taj",
-        address: 'tajmahal road md.pur',
-        phone: '01729948299'
-    });
+    const [user]=useAuthState(auth);
+//     const [user,setUser]=useState({
+//         name: 'Akbar the great ',
+//         email: "akbar@moomo.taj",
+//         address: 'tajmahal road md.pur',
+//         phone: '01729948299'
+//     });
 
-   const handleAddressChange =event =>{
-    //    const newvalue=event.target.value;
-      const {address,...rest}=user;
-      const newAddress =event.target.value;
-      const newUser ={address:newAddress,...rest};
-      console.log(newUser);
-      setUser(newUser);
-    //   console.log(address,rest);
+//    const handleAddressChange =event =>{
+//     //    const newvalue=event.target.value;
+//       const {address,...rest}=user;
+//       const newAddress =event.target.value;
+//       const newUser ={address:newAddress,...rest};
+//       console.log(newUser);
+//       setUser(newUser);
+//     //   console.log(address,rest);
       
-   }
+//    }
+
+const handlePlaceOrder =(event)=>{
+    event.preventDefault();
+    const order ={
+        email: user.email,
+        service: service.name,
+        servicesId : servicesId,
+        address: event.target.address.value,
+        phone: event.target.phone.value,
+    }
+
+}
+
+
     return (
         <div className='w-50 mx-auto'>
             <h2>Please order :{service.name}</h2>
-            <form >
-                <input className='w-100 mb-2' value={user.name} type="text"name="name" placeholder='Name' required />
+            <form onSubmit={handlePlaceOrder} >
+                <input className='w-100 mb-2' value={user.displayName} type="text"name="name" placeholder='Name' required  readOnly disabled/>
                 <br />
-                <input className='w-100 mb-2' value={user.email} type="email"name="email" placeholder='Email' required />
+                <input className='w-100 mb-2' value={user.email} type="email"name="email" placeholder='Email' required  readOnly disabled/>
                 <br />
                 <input className='w-100 mb-2' value={service.name} type="text"name="service" placeholder='service' required />
                 <br />
-                <input className='w-100 mb-2' onChange={handleAddressChange} value={user.address} type="text"name="address" placeholder='address' required />
+                <input className='w-100 mb-2' type="text"name="address" placeholder='address' required />
                 <br />
                 <input className='w-100 mb-2' value={user.phone} type="text"name="phone" placeholder='phone' required />
                 <br />
